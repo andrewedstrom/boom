0cd2737f03609493699db71ac9b3ab5ee671009c
Andrew Edstrom <andrewedstrom@gmail.com>
2021-02-09 10:34:47 -0800

    autoformat utils

--- utils.lua
+++ utils.lua
@@ -8,46 +8,47 @@ end
 function noop()
 end
 
-function tile_is_wall(x,y)
-	return fget(mget(x,y),0)
+function tile_is_wall(x, y)
+	return fget(mget(x, y), 0)
 end
 
 function blankmap()
-	local ret,x,y={}
-	for x=starting_x,starting_x+xmax do
-		ret[x]={}
-		for y=starting_y,starting_y+ymax do
-			ret[x][y]=false
+	local ret, x, y = {}
+	for x = starting_x, starting_x + xmax do
+		ret[x] = {}
+		for y = starting_y, starting_y + ymax do
+			ret[x][y] = false
 		end
 	end
 	return ret
 end
 
-function update_squares(x,y)
-	squares=blankmap()
+function update_squares(x, y)
+	squares = blankmap()
 
-	local lc,uc,rc,dc=true,true,true,true --continue flags
-	local candx,candy
-	for i=1,ymax do
+	--continue flags
+	local lc, uc, rc, dc = true, true, true, true
+	local candx, candy
+	for i = 1, ymax do
 		--left
 		if lc then
-			candx,candy=x-i,y
-			lc=los_continues(candx,candy)
+			candx, candy = x - i, y
+			lc = los_continues(candx, candy)
 		end
 		--right
 		if rc then
-			candx,candy=x+i,y
-			rc=los_continues(candx,candy)
+			candx, candy = x + i, y
+			rc = los_continues(candx, candy)
 		end
 		--up
 		if uc then
-			candx,candy=x,y-i
-			uc=los_continues(candx,candy)
+			candx, candy = x, y - i
+			uc = los_continues(candx, candy)
 		end
 		--down
 		if dc then
-			candx,candy=x,y+i
-			dc=los_continues(candx,candy)
+			candx, candy = x, y + i
+			dc = los_continues(candx, candy)
 		end
 
 		if not (lc or uc or rc or dc) then
@@ -57,11 +58,11 @@ function update_squares(x,y)
 end
 
 --line of sight
-function los_continues(x,y)
-	local is_wall=tile_is_wall(x,y)
+function los_continues(x, y)
+	local is_wall = tile_is_wall(x, y)
 	if not is_wall then
 		-- can see player from here
-		squares[x][y]=true
+		squares[x][y] = true
 	end
 
 	return not is_wall
